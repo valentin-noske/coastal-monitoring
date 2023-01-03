9e1c0aa4491d74968377d6e2938c84c6981fd724
valentin-noske <78734218+valentin-noske@users.noreply.github.com>
2023-01-03 23:14:30 +0100

    Panel created

--- unity_project/Assets/Own_assets/Moving_blackdots.cs
+++ unity_project/Assets/Own_assets/Moving_blackdots.cs
@@ -5,8 +5,6 @@ using System;
 
 public class Moving_blackdots : MonoBehaviour
 {
-    bool down = true;
-    float speed_y = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,20 +14,11 @@ public class Moving_blackdots : MonoBehaviour
     // Update is called once per frame0
     void Update()
     {
-    if(down){
-        transform.position = new Vector3(transform.position.x, Math.Max(transform.position.y - speed_y * Time.deltaTime, 0), transform.position.z);
-        speed_y = speed_y*2;
-        if(transform.position.y == 0){
-            down = false;
-        }
+    if(transform.position.y > -80){
+        transform.position -= transform.up * 0.05f;
     }
     else {
-        speed_y = speed_y/2;
-        transform.position = new Vector3(transform.position.x, Math.Min(transform.position.y + speed_y * Time.deltaTime, 5), transform.position.z);
-        if(transform.position.y == 5){
-            down = true;
-            speed_y = 0.01f;
-        }
+        transform.position += transform.up * 0.05f;
     }
 }
 }
